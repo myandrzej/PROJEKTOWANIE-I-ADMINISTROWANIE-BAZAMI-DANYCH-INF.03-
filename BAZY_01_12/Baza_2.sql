68ea15dc586c5d9b3e953f243f20839a90678e63
myandrzej <98713973+myandrzej@users.noreply.github.com>
2024-12-01 13:27:18 +0100

    Update Baza_2.sql

--- BAZY_01_12/Baza_2.sql
+++ BAZY_01_12/Baza_2.sql
@@ -1,52 +1,25 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Czas wygenerowania: 25 Wrz 2014, 08:08
--- Wersja serwera: 5.5.32
--- Wersja PHP: 5.4.19
+-- Tworzenie bazy danych
+CREATE DATABASE IF NOT EXISTS baza_druga;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-CREATE DATABASE IF NOT EXISTS quiz
-CHARACTER SET utf8
-COLLATE utf8_general_ci;
-USE quiz;
+-- Użycie bazy danych
+USE baza_druga;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+-- Tworzenie tabeli `pytania`
+CREATE TABLE IF NOT EXISTS pytania (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tresc TEXT NOT NULL,
+    odpa VARCHAR(255) NOT NULL,
+    odpb VARCHAR(255) NOT NULL,
+    odpc VARCHAR(255) NOT NULL,
+    odpd VARCHAR(255) NOT NULL,
+    answer CHAR(1) NOT NULL,
+    kategoria VARCHAR(255) NOT NULL,
+    rok INT NOT NULL
+);
 
---
--- Baza danych: `quiz`
---
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `pytania`
---
-
-CREATE TABLE IF NOT EXISTS `pytania` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tresc` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `odpa` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `odpb` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `odpc` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `odpd` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `answer` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `kategoria` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `rok` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=21 ;
-
---
--- Zrzut danych tabeli `pytania`
---
-
-INSERT INTO `pytania` (`id`, `tresc`, `odpa`, `odpb`, `odpc`, `odpd`, `answer`, `kategoria`, `rok`) VALUES
+-- Wstawianie danych
+INSERT INTO `pytania` (`id`, `tresc`, `odpa`, `odpb`, `odpc`, `odpd`, `answer`, `kategoria`, `rok`) 
+VALUES
 (1, 'Wypisanie na ekranie zawartosci zmiennej char s = "To jest tylko test" zostanie wykonane w jezyku C++ za pomoca instrukcji', 'cin<<s', 'cout>>s', 'cout<<s', 'cin>>s', 'c', 'programowanie', 2012),
 (2, 'Destruktor w jezyku C++ to metoda', 'wywolywana w momencie usuwania obiektu', 'wywolywana w momencie tworzenia obiektu', 'zwalniajaca pamiec przydzielona dynamicznie w obiekcie', 'porzadkujaca pamiec operacyjna po usunietych obiektach', 'a', 'programowanie', 2009),
 (3, 'Dziedziczenie w programowaniu obiektowym pozwala na', 'laczenie obiektow', 'kopiowanie cech jednego obiektu do innego', 'usuniecie z istniejacej klasy zbednych elementow.', 'tworzenie nowej klasy na podstawie jednej lub kilku juz istniejacych klas', 'd', 'programowanie', 2008),
@@ -61,10 +34,4 @@ INSERT INTO `pytania` (`id`, `tresc`, `odpa`, `odpb`, `odpc`, `odpd`, `answer`, 
 (12, 'Jaka ilosc rzeczywistych danych mozna przeslac w czasie 1 s przez lacze synchroniczne o przepustowosci 512 kbps, bez sprzetowej i programowej kompresji?', 'Okolo 5 kB', 'Okolo 55 kB', 'Ponad 64 kB', 'Ponad 500 kB', 'b', 'systemy operacyjne i sieci', 2012),
 (13, 'Jakiej liczbie kolorow odpowiada kolor zakodowany na 16 bitach?', '16 kolorom', 'okolo 64 tysiacom kolorow', 'okolo 65 tysiacom kolorow', 'okolo 16 milionom kolorow', 'c', 'multimedia i grafika', 2008),
 (14, 'Pamiec oznaczona symbolem PC3200 nie moze wspolpracowac z magistrala', '300 MHz', '333 MHz', '400 MHz', '533 MHz', 'd', 'urzadzenia techniki komp.', 2006),
-(15, 'Co oznacza jednostka dpi podawana w parametrach katalogowych skanerow i drukarek?', 'Punkty na cal', 'Gestosc optyczna', 'Punkty na milimetr', 'Punkty na centymetr', 'a', 'urzadzenia techniki komp.', 2009),
-(16, 'Jaka jest maksymalna predkosc odczytu plyt CD-R w napedzie oznaczonym x48?', '480 kB/s', '4800 kB/s', '7200 kB/s', '10000 kB/s', 'c', 'urzadzenia techniki komp.', 2006),
-(17, 'Funkcje roznych kategorii (daty i czasu, finansowe, tekstowe, matematyczne, statystyczne) sa elementem skladowym', 'edytora tekstu', 'przegladarki internetowej', 'arkusza kalkulacyjnego', 'programow do tworzenia prezentacji multimedialnych', 'c', 'oprogramowanie biurowe', 2010),
-(18, 'Ktory z podanych jezykow programowania nie jest jezykiem webowym?', 'HTML5', 'JavaScript', 'PHP', 'C++', 'd', 'programowanie', 2012),
-(19, 'W jezyku C++ roznica miedzy funkcja a procedura polega na tym, ze', 'funkcja zwraca wartosc, a procedura nie', 'do funkcji jako parametr mozna przekazywac zmienna, a do procedur tylko wartosc', 'do procedury jako parametr mozna przekazywac zmienna, a do funkcji tylko wartosc', 'funkcja moze byc zdefiniowana z tylko jednym parametrem, a procedura z wieloma', 'a', 'programowanie', 2006),
-(20, 'Jaki jest kod koloru w jezyku HTML, jesli kolor ten ma skladowe w zapisie dziesietnym: czerwony = 27, zielony = 90 i niebieski = 104?', '#279004', '#1B5A68', '#1C9D4A', '#5A681B', 'b', 'multimedia i grafika', 2010);
-
+(15, 'Co oznacza jednostka dpi podawana w parametrach katalogowych skanerow i drukarek?', 'Punkty na cal', 'Gestosc optyczna', 'Punkty na milimetr', 'Punkty na centymetr', 'a', 'urzadzenia techniki komp.', 2009);
