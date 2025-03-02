0e321deee80afa67e033c282345a12fa6341bf3b
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-03-02 08:59:13 +0100

    Update rozwiazania.sql

--- BAZY_02_03/inf_03_2023_01_01_SG/rozwiazania.sql
+++ BAZY_02_03/inf_03_2023_01_01_SG/rozwiazania.sql
@@ -1,2 +1,11 @@
 -- Zadanie 1
 SELECT cena, pojemnosc FROM farby WHERE kolor = 'czerwony';
+
+-- Zadanie 2
+SELECT kolor, cena/pojemnosc AS 'cena jednostkowa' FROM farby;
+
+-- Zadanie 3
+SELECT farby.kolor, malowanie.liczba_puszek FROM farby JOIN malowanie ON malowanie.id_farby = farby.id_farby WHERE malowanie.id_pomieszczenia = 3; 
+
+-- Zadanie 4
+CREATE VIEW pomieszczenie1 AS SELECT id_sciany, id_farby, liczba_puszek FROM malowanie WHERE id_pomieszczenia = 1;
