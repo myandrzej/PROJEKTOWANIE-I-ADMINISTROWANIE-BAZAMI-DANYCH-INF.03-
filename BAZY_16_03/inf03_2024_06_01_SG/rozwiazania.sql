d2279397fbe1408fdb470a3ebbe934fa8637bfb0
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-03-16 11:01:55 +0100

    Update rozwiazania.sql

--- BAZY_16_03/inf03_2024_06_01_SG/rozwiazania.sql
+++ BAZY_16_03/inf03_2024_06_01_SG/rozwiazania.sql
@@ -1,11 +1,10 @@
 -- Zadanie 1
 SELECT nazwa FROM wycieczki WHERE poczatek='Muszyna' OR poczatek='Wieliczka';
 
-
-
-
 -- Zadanie 2
 
-
+SELECT wycieczki.nazwa, wycieczki.opis, wycieczki.poczatek, zdjecia.zrodlo 
+FROM wycieczki JOIN zdjecia ON zdjecia_id = zdjecia.id;
 
 -- Zadanie 3
+SELECT COUNT(*) FROM wycieczki;
