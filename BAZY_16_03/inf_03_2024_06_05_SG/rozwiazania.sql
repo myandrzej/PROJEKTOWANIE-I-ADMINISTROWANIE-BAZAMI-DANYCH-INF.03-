6c097be8532a952d0decc33d05f74fb9a1543fae
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-03-16 12:56:58 +0100

    Update rozwiazania.sql

--- BAZY_16_03/inf_03_2024_06_05_SG/rozwiazania.sql
+++ BAZY_16_03/inf_03_2024_06_05_SG/rozwiazania.sql
@@ -1,7 +1,17 @@
 -- Zadanie 1
 
+SELECT nazwa FROM marki;
+
 -- Zadanie 2
 
+SELECT model, rocznik, przebieg, paliwo, cena, zdjecie FROM samochody WHERE id=10;
+
 -- Zadanie 3
+SELECT marki.nazwa, samochody.model, samochody.rocznik, samochody.cena, samochody.zdjecie FROM samochody 
+JOIN marki ON samochody.marki_id = marki.id 
+WHERE samochody.wyrozniony=1 LIMIT 4;
 
 -- Zadanie 4
+SELECT samochody.model, samochody.cena, samochody.zdjecie FROM samochody 
+JOIN marki ON samochody.marki_id = marki.id 
+WHERE marki.nazwa = 'Audi';
