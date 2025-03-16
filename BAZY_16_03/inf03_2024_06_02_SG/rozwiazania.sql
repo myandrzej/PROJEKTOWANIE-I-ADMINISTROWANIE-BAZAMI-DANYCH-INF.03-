ef44b3a3d72657d291c63a2f6e14a23add53d48b
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-03-16 11:30:22 +0100

    Update rozwiazania.sql

--- BAZY_16_03/inf03_2024_06_02_SG/rozwiazania.sql
+++ BAZY_16_03/inf03_2024_06_02_SG/rozwiazania.sql
@@ -5,3 +5,15 @@ INSERT INTO logowanie(id, nick, haslo) VALUES (3, 'Jeremi', 'Jer123');
 -- Zadanie 2
 
 SELECT FLOOR(AVG(rokUr)) AS 'Średni rok urodzenia' FROM uczestnicy;
+
+-- Zadanie 3
+
+SELECT imie, nazwisko, nick, haslo FROM uczestnicy JOIN logowanie ON uczestnicy.logowanie_id = logowanie.id WHERE imie LIKE "J%";
+
+
+-- Zadanie 4
+
+CREATE USER 'uczestnik'@'localhost' IDENTIFIED BY 'Ucz123&';
+
+-- Zadanie 5
+GRANT SELECT, UPDATE ON chat.uczestnicy TO 'uczestnik'@'localhost';
