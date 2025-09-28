f09f0f5b3bef82fcafbe7aca9538ebe4fc936e1c
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-09-28 11:32:06 +0200

    Update wyprawy-odpowiedzi.sql

--- BAZY_28_09/INF.03-07-25.06-SG/wyprawy-odpowiedzi.sql
+++ BAZY_28_09/INF.03-07-25.06-SG/wyprawy-odpowiedzi.sql
@@ -10,6 +10,11 @@ SELECT nazwa, cena, link_obraz FROM miejsca WHERE link_obraz LIKE '0%';
 -- Zapytanie 4: wybierające jedynie nazwy wszystkich miejsc i odpowiadające im obliczone
 -- średnie liczby osób dorosłych, zgrupowane według nazw miejsc. Średnia liczba osób jest
 -- zaokrąglona do liczby całkowitej. Należy posłużyć się relacją
+SELECT nazwa, ROUND(AVG(liczba_doroslych)) AS 'Średnia liczba' 
+FROM miejsca 
+JOIN wycieczki ON miejsca.id_miejsca = wycieczki.id_miejsca 
+GROUP BY nazwa;
+
 -- Zapytanie 5: tworzące tabelę klienci z kolumnami:
 -- − id_klienta jako liczba całkowita dodatnia, klucz podstawowy, autoinkrementowany, nie może
 -- przyjmować wartości NULL
