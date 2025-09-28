6dba69cbba538d29f054a1f538ab897483d357d9
myandrzej <98713973+myandrzej@users.noreply.github.com>
2025-09-28 11:26:17 +0200

    Update wyprawy-odpowiedzi.sql

--- BAZY_28_09/INF.03-07-25.06-SG/wyprawy-odpowiedzi.sql
+++ BAZY_28_09/INF.03-07-25.06-SG/wyprawy-odpowiedzi.sql
@@ -5,14 +5,15 @@ SELECT nazwa FROM miejsca ORDER BY nazwa ASC;
 SELECT cena FROM miejsca WHERE nazwa = 'Malbork';
 
 -- Zapytanie 3: wybierające jedynie nazwę, cenę i link obrazu z tabeli miejsca dla wierszy,
-w których link obrazu rozpoczyna się od 0 (zero)
+-- w których link obrazu rozpoczyna się od 0 (zero)
+SELECT nazwa, cena, link_obraz FROM miejsca WHERE link_obraz LIKE '0%';
 -- Zapytanie 4: wybierające jedynie nazwy wszystkich miejsc i odpowiadające im obliczone
-średnie liczby osób dorosłych, zgrupowane według nazw miejsc. Średnia liczba osób jest
-zaokrąglona do liczby całkowitej. Należy posłużyć się relacją
+-- średnie liczby osób dorosłych, zgrupowane według nazw miejsc. Średnia liczba osób jest
+-- zaokrąglona do liczby całkowitej. Należy posłużyć się relacją
 -- Zapytanie 5: tworzące tabelę klienci z kolumnami:
-− id_klienta jako liczba całkowita dodatnia, klucz podstawowy, autoinkrementowany, nie może
-przyjmować wartości NULL
-− imie typu tekstowego o długości do 20 znaków
-− nazwisko typu tekstowego o długości do 40 znaków
-− data_ur typu data
+-- − id_klienta jako liczba całkowita dodatnia, klucz podstawowy, autoinkrementowany, nie może
+-- przyjmować wartości NULL
+- − imie typu tekstowego o długości do 20 znaków
+-- − nazwisko typu tekstowego o długości do 40 znaków
+-- − data_ur typu data
 
